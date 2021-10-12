de61b3aa1560460b5b79dd1cbb930471624b078d
Ademu Anthony <ademuanthony@gmail.com>
2021-10-12 11:04:43 +0100

    area of practice

---
+++ src/Legalpedia.EntityFrameworkCore/Migrations/20211012035318_ProfileDetails.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Legalpedia.Migrations
+{
+    public partial class ProfileDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Intagram",
+                table: "AbpUsers",
+                newName: "Website");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AreaOfPractice",
+                table: "AbpUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CallToBarYear",
+                table: "AbpUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instagram",
+                table: "AbpUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AreaOfPractice",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CallToBarYear",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Instagram",
+                table: "AbpUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Website",
+                table: "AbpUsers",
+                newName: "Intagram");
+        }
+    }
+}
